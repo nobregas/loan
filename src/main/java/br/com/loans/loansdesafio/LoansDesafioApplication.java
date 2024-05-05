d2fa7bb67753a386a17b3f5b419f20124a48d945
nobregas <gabrielnobregasantos2@gmail.com>
2024-05-05 20:41:33 -0300

    feat: initialize project

---
+++ src/main/java/br/com/loans/loansdesafio/LoansDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.loans.loansdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansDesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansDesafioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/loans/loansdesafio/LoansDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loans.loansdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansDesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
